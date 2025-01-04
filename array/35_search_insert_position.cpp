17843a1272a31b280b8a9081a1be7ec8634a83cf
Md. Tuser Sheikh <mdtusersh@gmail.com>
2025-01-04 11:44:36 +0600

    27 solved

---
+++ array/35_search_insert_position.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int left = 0;
+        int right = nums.size() - 1;
+        int mid = 0;
+
+        if(target > nums[right]) {
+            return right + 1;
+        }
+
+        while(left <= right) {
+            mid = left + (right - right) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+
+            if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return mid;
+    }
+};
