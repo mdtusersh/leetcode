9ae00ff8d1361cbbc81558d9fa0f03658e734ae5
Md. Tuser Sheikh <mdtusersh@gmail.com>
2025-02-03 22:44:37 +0600

    1991 solved

---
+++ array/1991_find_the_middle_index_in_array.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int findMiddleIndex(vector<int>& nums) {
+        int sum = 0;
+        for(int n: nums) {
+            sum += n;
+        }
+
+        int pre = 0;
+        for(int i = 0; i < nums.size(); i++) {
+            if(pre == sum - pre - nums[i]) {
+                return i;
+            }
+
+            pre += nums[i];
+        }
+
+        return -1;
+    }
+};
